4cf2c6a01f54e9a1988788079018ed5ada59527d
Comartsau <comartsau@gmail.com>
2024-07-13 20:13:55 +0700

    Update header.tsx

--- app/components/home/header.tsx
+++ app/components/home/header.tsx
@@ -115,14 +115,14 @@ export function HeaderHome() {
       <Navbar className="sticky min-w-full top-0 z-10 h-max rounded-none px-4 py-1 lg:px-8  ">
         <div className="flex w-full items-center justify-between text-blue-gray-900">
           <div className=" flex items-center w-[150px] h-[40px] ">
-            <Image
+            {/* <Image
               src="/logo1.png"
               alt="logo"
               onClick={() => router.push("/")}
               width={70}
               height={50}
               className=" rounded-lg cursor-pointer  "
-            />
+            /> */}
             <Typography className=" py-1.5 font-medium text-2xl  whitespace-nowrap">
               คอร์สออนไลน์
             </Typography>
